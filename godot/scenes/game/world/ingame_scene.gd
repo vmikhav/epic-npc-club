da2406efbe86a54635c6f446990c44b069f7081b
vmikhav <mihaw.wolodymyr@gmail.com>
2024-01-28 15:17:29 +0200

    finish story

--- godot/scenes/game/world/ingame_scene.gd
+++ godot/scenes/game/world/ingame_scene.gd
@@ -4,11 +4,18 @@ extends Node2D
 @onready var pause_overlay = %PauseOverlay
 @onready var inventory = $InventoryStacked as InventoryStacked
 var intro = preload("res://dialogues/intro.dialogue")
+var baradumDialogue = preload("res://dialogues/baradum.dialogue")
 var firstDialogue = preload("res://dialogues/first.dialogue")
+var routineDialogue = preload("res://dialogues/routine.dialogue")
+var finalDialogue = preload("res://dialogues/final.dialogue")
+
+var quests_limit = 10
+var dialogues_number = 0
+var is_final = false
 
 func _ready() -> void:
-	#inventory.set_item_stack_size(inventory.get_item_by_id('fish'), 5)
 	fade_overlay.visible = true
+	Events.gold = 500
 	
 	if SaveGame.has_save():
 		SaveGame.load_game(get_tree())
@@ -17,6 +24,20 @@ func _ready() -> void:
 	get_tree().create_timer(1.5).timeout.connect(func():
 		DialogueManager.show_dialogue_balloon(intro)
 		DialogueManager.dialogue_ended.connect(dialogue_ended)
+	)
+	Events.update_inventory.connect(func(item, amount):
+		var _current_amount = inventory.get_item_stack_size(inventory.get_item_by_id(item))
+		if item == 'gold':
+			_current_amount = Events.gold
+		_current_amount += amount
+		inventory.set_item_stack_size(inventory.get_item_by_id(item), _current_amount)
+		Events[item] = _current_amount
+		
+		if item == 'gold':
+			print('money paid ', amount, ', total ', Events.gold, ', ', inventory.get_item_stack_size(inventory.get_item_by_id(item)))
+			quests_limit -= 1
+			if _current_amount < 30:
+				quests_limit = 0
 	)
 
 func _input(event) -> void:
@@ -30,6 +51,22 @@ func _save_game() -> void:
 	SaveGame.save_game(get_tree())
 
 func dialogue_ended(resource):
+	if is_final:
+		get_tree().change_scene_to_file("res://scenes/menu/main_menu_scene.tscn")
+		return
+	dialogues_number += 1
 	if resource == intro:
 		%CtrlInventoryStacked.show()
-		print('intro ended')
+	
+	await get_tree().create_timer(randf_range(1.5, 3)).timeout
+	if dialogues_number == 1:
+		DialogueManager.show_dialogue_balloon(firstDialogue)
+	elif dialogues_number == 6:
+		DialogueManager.show_dialogue_balloon(baradumDialogue)
+		$Music.play()
+	elif quests_limit <= 0:
+		Events.calc_score()
+		is_final = true
+		DialogueManager.show_dialogue_balloon(finalDialogue)
+	else:
+		DialogueManager.show_dialogue_balloon(routineDialogue)
--- godot/ui/components/events.gd
+++ godot/ui/components/events.gd
@@ -4,5 +4,87 @@ extends Node
 signal character_started_talking(character_name)
 signal character_finished_talking(character_name)
 
+signal update_inventory(id: String, amount: int)
 
-var fish_count = 5
+var strength = 5
+var constitution = 5
+var dexterity = 5
+var intelligence = 5
+var wisdom = 5
+var charisma = 5
+var level = 1
+var quest_cost = 20
+var quest_result = 0
+
+var gold = 500
+var food = 0
+var chair = 0
+var joke = 0
+var invitation = 0
+var total_score = 0
+
+func generate_adventurer():
+	level = randi_range(1, 15)
+	quest_cost = 20 + level * 3
+	var points_pool = 4*level
+	var _point = 0
+	_point = randi_range(0, min(17, points_pool))
+	points_pool -= _point
+	strength = 3 + _point
+	_point = randi_range(0, min(17, points_pool))
+	points_pool -= _point
+	constitution = 3 + _point
+	_point = randi_range(0, min(17, points_pool))
+	points_pool -= _point
+	dexterity = 3 + _point
+	_point = randi_range(0, min(17, points_pool))
+	points_pool -= _point
+	intelligence = 3 + _point
+	_point = randi_range(0, min(17, points_pool))
+	points_pool -= _point
+	wisdom = 3 + _point
+	_point = randi_range(0, min(17, points_pool))
+	points_pool -= _point
+	charisma = 3 + _point
+
+func process_food_quest():
+	var result = randi_range(1, 20) + max(ability_bonus(dexterity), ability_bonus(intelligence))
+	quest_result = max(0, result)
+	print(quest_result)
+	if quest_result > 0:
+		update_inventory.emit('food', quest_result)
+	return quest_result
+
+func process_chair_quest():
+	var result = randi_range(1, 20) + max(ability_bonus(strength), ability_bonus(constitution))
+	quest_result = max(0, result)
+	print(quest_result)
+	if quest_result > 0:
+		update_inventory.emit('chair', quest_result)
+	return quest_result
+
+func process_joke_quest():
+	var result = randi_range(1, 20) + max(ability_bonus(charisma), ability_bonus(wisdom))
+	quest_result = max(0, result)
+	print(quest_result)
+	if quest_result > 0:
+		update_inventory.emit('joke', quest_result)
+	return quest_result
+
+func process_invitation_quest():
+	var result = randi_range(1, 20) + max(ability_bonus(charisma), ability_bonus(constitution))
+	quest_result = max(0, result)
+	print(quest_result)
+	if quest_result > 0:
+		update_inventory.emit('invitation', quest_result)
+	return quest_result
+
+func pay_reward():
+	update_inventory.emit('gold', -quest_cost)
+
+func ability_bonus(points: int) -> int:
+	return (points - 10) / 2
+
+func calc_score():
+	var _visitors = min(invitation, chair)
+	var total_score = round(joke * (_visitors - min(0, food - _visitors) * .25) + max(0, invitation - _visitors) * .25)
